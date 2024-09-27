78bbbab5ac72e53501a5655fd9236a2523637752
tticasso <takhanhtung31@gmail.com>
2024-09-28 02:22:39 +0700

    update

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,12 +1,12 @@
 import express  from "express"
 import cors from 'cors'
-import { connectDB } from "./config/db.js"
-import userRouter from "./routes/userRoute.js"
-import foodRouter from "./routes/foodRoute.js"
+import { connectDB } from "../config/db.js"
+import userRouter from "../routes/userRoute.js"
+import foodRouter from "../routes/foodRoute.js"
 import 'dotenv/config'
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
-import blogRouter from "./routes/blogRoute.js"
+import cartRouter from "../routes/cartRoute.js"
+import orderRouter from "../routes/orderRoute.js"
+import blogRouter from "../routes/blogRoute.js"
 
 // app config
 const app = express()
