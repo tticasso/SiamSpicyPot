b19b6e05c12f3a54884c1b40faf6dcd37650a15d
tticasso <135634447+tticasso@users.noreply.github.com>
2024-09-28 02:17:59 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import blogRouter from "./routes/blogRoute.js"
 
 // app config
 const app = express()
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 
 // middlewares
